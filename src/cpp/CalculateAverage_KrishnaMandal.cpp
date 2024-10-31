f3a6715269d401cfcb0de901a8cda123ba16d386
Krishna Kumar Mandal <krishiway@gmail.com>
2024-10-31 14:08:46 +0100

    very first attempt

--- src/cpp/CalculateAverage_KrishnaMandal.cpp
+++ src/cpp/CalculateAverage_KrishnaMandal.cpp
@@ -2,4 +2,24 @@
 // Created by mfw-150 on 10/31/24.
 //
 
-#include "CalculateAverage_KrishnaMandal.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string fileName = "../measurements.txt";
+
+int main() {
+    std::ifstream file(fileName); // Open the file
+    if (!file) {
+        std::cerr << "Unable to open file\n";
+        return 1; // Return an error code
+    }
+
+    std::string line;
+    while (std::getline(file, line)) { // Read the file line by line
+        std::cout << line << '\n'; // Print each line
+    }
+
+    file.close(); // Close the file
+    return 0;
+}
