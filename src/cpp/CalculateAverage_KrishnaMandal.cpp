b6702be5ada79d33a6d35d3d32d39e6493075d4a
Krishna Kumar Mandal <krishiway@gmail.com>
2024-11-04 11:33:32 +0100

    Added buffer size to read chunk of memory

--- src/cpp/CalculateAverage_KrishnaMandal.cpp
+++ src/cpp/CalculateAverage_KrishnaMandal.cpp
@@ -4,22 +4,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
 
 std::string fileName = "../measurements.txt";
 
 int main() {
-    std::ifstream file(fileName); // Open the file
+    std::ifstream file(fileName, std::ios::in | std::ios::binary);
     if (!file) {
-        std::cerr << "Unable to open file\n";
-        return 1; // Return an error code
+        std::cerr << "Unable to open file 'measurement.txt'\n";
+        return 1;
     }
 
+    constexpr  size_t BUFFER_SIZE = 1024 * 1024; // 1 MB buffer
+    std::vector<char> buffer(BUFFER_SIZE);
     std::string line;
-    while (std::getline(file, line)) { // Read the file line by line
-        std::cout << line << '\n'; // Print each line
+    size_t line_count = 0;
+
+    while (file.read(buffer.data(), buffer.size()) || file.gcount() > 0) {
+        size_t bytes_read = file.gcount();
+        for (size_t i = 0; i < bytes_read; ++i) {
+            if (buffer[i] == '\n') {
+                ++line_count;
+                // calculate min, max, average
+            }
+        }
     }
 
-    file.close(); // Close the file
+    file.close();
+    std::cout << "Total lines read: " << line_count << "\n";
     return 0;
 }
